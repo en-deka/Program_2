d487783439ccfd740aabf381bb74bbd1df0db3b4
Endeka <allisonfischer@lewisu.edu>
2020-08-14 21:06:39 -0400

    Update Figures.java

--- src/Figures.java
+++ src/Figures.java
@@ -1,15 +1,20 @@
+import java.util.*;
 
-//   Figures.java      Author: Allison Fischer
+//**************************************************************************************************
+//  Figures.java      Author: Allison Fischer
 //  Summer 2020		PROGRAMMING ASSIGNMENT 2
+//  Final revision August 14, 2020
 //  July 31, 2020
 //  This program asks the user to enter the size of a figure (must be odd). 
 //  It then displays a menu of 4 options. Options 1-3 will print various figures: box, diamond, X. 
 //  Option 4 will allow the user to quit the program.*/
+//**************************************************************************************************
 
-import java.util.Scanner;
-import java.lang.*;
+
 
 public class Figures {
+	
+
 
 	static int size = 0; // Initializes int odd_number which will be the size of the figure.
 	int option_choice; // Initializes int option choice to allow user to choose a figure or quit
@@ -27,18 +32,14 @@ public class Figures {
 	}
 // Method for printing diamond
 	static void printDiamond(int size) {
-		for (int row = 1; row <= size; row++) {
-			for (int columns = 1; columns <= size; columns++) {
-				if (row == columns) {
-					System.out.print("X");
-				} else if (row + columns == size + 1) {
-					System.out.print(" ");
-				} else {
-					System.out.print(" ");
-				}
-			}
-			System.out.print("\n");
-		}
+		
+		for (int row = 1; row <= 0.5 * (size + 1); row++) {
+			for (int columns = 1; columns <= (size + 1); columns++) {
+				size = 2 * (columns + row - 1);
+		for (size = 2 * (columns + row - 1); size < size; row++ ) {
+			System.out.println("X");
+		}}}
+	
 
 	}
 
@@ -61,6 +62,7 @@ public class Figures {
 	}
 
 	public static void main(String[] args) {
+	
 				System.out.println("Programming Fundamentals \nNAME: Allison Fischer \nPROGRAMMING ASSIGNMENT 2");
 				System.out.println("\nPlease enter the size of the figure you would like created.\r\nThe size must be"
 						+ " an odd number:  ");	//Prompts the user to enter the size of the figure (odd number).
@@ -81,7 +83,8 @@ public class Figures {
 					if (option_choice == 2)
 					{printDiamond(size);}
 					if (option_choice == 3)
-					{printX(size);
+					{printX(size);}
 					if (option_choice == 4)
-						System.out.print("Good bye!");}
-					System.exit(0);}
+						{System.out.print("Good bye!");
+						System.exit(0);}}
+					}
